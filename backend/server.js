e755f5185ac75fd72ddebf1b97a3e9a2f9eab595
dewang28 <dewangbaluni28@gmail.com>
2025-11-08 14:41:03 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/tasks', tasksRouter)
 
 const start = async () => {
 await mongoose.connect(process.env.MONGO_URI )
-app.listen(process.env.PORT || 5000)
-console.log(`Server running on port ${process.env.PORT || 5000}` )
+app.listen(process.env.PORT || 4000)
+console.log(`Server running on port ${process.env.PORT || 4000}` )
 }
 start()
